4de253d0796d2a23fb26b67b822af7124b72629e
SuhasParameshwara <suhas.parameshwara@outlook.com>
2017-08-16 00:41:51 -0700

    Upload Code
    Challenging Problems

---
+++ ThreeSumOfDigits.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ThreeSumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = BigInteger.Pow(2, 1000);
+            char[] charNum = number.ToString().ToCharArray();
+            int len = charNum.Length;
+            double sum = 0;
+            for(int i=0; i < len; i++)
+            {
+                sum += Char.GetNumericValue(charNum[i]);
+            }
+            Console.WriteLine("The sum of the digits of the number 2^1000 is "+sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TwoFibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstNum = 1,SecondNum = 2,evenSum = 2,sum = 0;
+            while(FirstNum + SecondNum < 4000000)
+            {
+                sum = FirstNum + SecondNum;
+                FirstNum = SecondNum;
+                SecondNum = sum;
+                if(sum % 2 == 0)
+                {
+                    evenSum += sum;
+                }
+            }
+            Console.WriteLine("The sum of even valued terms in Fibonacci series till 4000000 is " + evenSum);
+            Console.ReadLine();
+        }
+    }
+}
